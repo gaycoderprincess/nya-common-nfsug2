dd46f2e631f71c7d5bebfbcbf93487d0c1a301f9
💖 Chloe 💖 <gaycoderprincess@proton.me>
2026-02-05 18:55:37 +0100

    player transmission bool

--- types/Player.h
+++ types/Player.h
@@ -1,8 +1,12 @@
 class Player {
 public:
-	uint8_t _0[0x50];
+	uint8_t _0[0x20];
+	bool bManualTransmission; // +20
+	uint8_t _21[0x2F];
 	bool bFinishedRacing; // +50
 
 	static inline auto pPlayersByIndex = (Player**)0x8900B4;
 	static inline auto& NumPlayers = *(int*)0x8900BC;
 };
+static_assert(offsetof(Player, bManualTransmission) == 0x20);
+static_assert(offsetof(Player, bFinishedRacing) == 0x50);
